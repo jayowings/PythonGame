3e651ed6dabe819b1e364d08f8ef63fda65c304b
Jay Owings <jaydenowings@mail.weber.edu>
2021-02-10 08:52:42 -0700

    Player selection initial capabilities

--- Game.py
+++ Game.py
@@ -1,2 +1,22 @@
 # Author: Jayden Owings
 # Feb 10, 2021
+
+#This is a game to demo functions in python
+
+# Rock Paper Scissors
+
+# Rock beats Scissors
+# Scissors beat Paper
+# Paper beats Rock
+
+#Pseudo Code --> 
+
+# User selection -- get info from user
+# Computer selection -- find some way for the computer to select the options
+# Evaluate the results
+# Declare a winner
+
+print('What is your selection? (Rock, Paper, Scissors):')
+player = input()
+#TODO Validate user selection
+print ('You chose', player)
