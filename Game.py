7f1391b5f9b0d36621bd085e3965ea6fe61d104c
Jay Owings <jaydenowings@mail.weber.edu>
2021-02-17 09:01:25 -0700

    added while loop and cheat code

--- Game.py
+++ Game.py
@@ -2,64 +2,57 @@ import random
 a = 1
 possible_choices = ['Rock','Paper','Scissors']
 
-# User selection -- get info from user
-print('What is your selection? (Rock, Paper, Scissors):')
-player = input()
-#TODO Validate user selection
-print ('You chose', player)
-
-#------Cheat-----
-if player == 'Dragon':
-    print ("AHHHH! A Dragon! Run for your life!")
-    if a == a:
-        if a < a:
-            print (" ")
-        else:
-            print (" ")
-    print(" ")
-    print ("...")
-    print ("I think you've won")
-else:
-# Computer selection -- find way for the computer to select the options
-#   computer_selection = random.choices(possible_choices) #TODO uncomment for production
-
-
-#TODO remove this temp test
-
-
-    computer_selection = 'Paper'
-    print ('Computer chose', str(computer_selection))
-
-# Evaluate the results --conditional statement
-    if computer_selection == player:
-        print ("It's a tie!")
-
-#------Rock-----
-    elif player == 'Rock':
-    #computer selects Paper
-        if computer_selection == 'Paper':
-            print ('You lose!')
-    #computer selects Scissors
-        else:
-            print ("You win!")
-
-#------Paper-----
-    elif player == 'Paper':
-        #computer selects Rock
-        if computer_selection == 'Rock':
-            print ('You win!')
-    #computer selects Scissors
-        else:
-            print ("You lose!")
-
-#------Scissors-----
+# loop through code until user wants to quit
+while True:
+    # User selection -- get info from user
+    print('What is your selection? (Rock, Paper, Scissors):')
+    player = input()
+    #TODO Validate user selection
+    print ('You chose', player)
+    #-----------Cheat--------
+    if player == 'Dragon':
+        print ("AHHHH! A Dragon! Run for your life!")
+        if a == a:
+            if a < a:
+                print (" ")
+            else:
+                print (" ")
+        print(" ")
+        print ("...")
+        print ("I think you've won")
     else:
-    #computer selects Rock
-        if computer_selection == 'Rock':
-            print ('You lose!')
-    #computer selects Paper
+        # Computer selection -- find way for the computer to select the options
+        computer_selection = random.choice(possible_choices)
+        print ('Computer chose', str(computer_selection))
+        # Evaluate the results --conditional statement
+        if computer_selection == player:
+            print ("It's a tie!")
+            #----------------------Rock-------------------------
+        elif player == 'Rock':
+            #computer selects Paper
+            if computer_selection == 'Paper':
+                print ('You lose!')
+                #computer selects Scissors
+            else:
+                print ("You win!")
+    #------------------------Paper----------------------------
+        elif player == 'Paper':
+            #computer selects Rock
+            if computer_selection == 'Rock':
+                print ('You win!')
+            #computer selects Scissors
+            else:
+                print ("You lose!")
+    #-----------------------Scissors----------------------
         else:
-            print ("You win!")
+        #computer selects Rock
+            if computer_selection == 'Rock':
+                print ('You lose!')
+        #computer selects Paper
+            else:
+                print ("You win!")
+    play_again = input("Do you want to play again (y/n): ")
+    if play_again == 'n':
+        break
 
-# Declare a winner
-
+print ('Thanks for playing')
